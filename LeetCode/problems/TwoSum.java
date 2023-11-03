e4709b1d24dc912fcd9c87e076a28efe63ca7d0c
Artem Yuran <yuoran371@gmail.com>
2023-11-03 20:23:31 +0300

    Add solution via int[] (just like SQL wants)

--- LeetCode/problems/TwoSum.java
+++ LeetCode/problems/TwoSum.java
@@ -3,6 +3,7 @@ package problems;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 import java.lang.Integer;
 
 
@@ -10,17 +11,15 @@ import java.lang.Integer;
 // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 // You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
-public class TwoSum {
+class TwoSum {
 	public ArrayList<Integer> sum(ArrayList<Integer> al, int a) {
 		ArrayList<Integer> arrayList = new ArrayList<>();
 		Integer g = Integer.valueOf(4);
 		for (int i=0; i<al.size()-1; i++) {
 			for (int j=i+1; j<al.size(); j++) {
 				if ((al.get(i)+al.get(j))==a) {
-//					if (al.get(i)==arrayList.get(i)&&al.get(j)==arrayList.get(j)) {
 					arrayList.add(i); 
 					arrayList.add(j); 
-//					}
 				}
 			}
 		}
@@ -28,8 +27,24 @@ public class TwoSum {
 	}
 }
 
+class SolutionTwoSum {
+public int[] twoSum (int[] nums, int target) {
+		int[]arr= new int[2];
+        for (int i=0; i<nums.length-1; i++) {
+        	for (int j=i+1; j<nums.length; j++) {
+        		if (nums[i]+nums[j] == target) {
+        			arr[0]=i;
+        			arr[1]=j;
+        		}
+        	}
+        }
+		return arr;
+    }
+}
+
 class Main {
 	public static void main(String[] args) {
+		
 		ArrayList<Integer> arrayList = new ArrayList<>();
 		arrayList.add(15);
 		arrayList.add(7);
