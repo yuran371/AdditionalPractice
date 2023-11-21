9a76183b9ca66cfd2c8bf4d291bc9142e0506773
Artem Yuran <yuoran371@gmail.com>
2023-11-21 16:52:05 +0300

    Add some solutions of Stream flatMap() List

--- oreillyJava8/streams/FlatMapTask.java
+++ oreillyJava8/streams/FlatMapTask.java
@@ -1,14 +1,23 @@
 package streams;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 class Phone {
 	private String name;
 	private int price;
+	private List<String> owners;
 	
-	Phone(String name, int price) {     // For example
+	Phone(String name, int price) {   		   // for first example
 		this.name = name;
 		this.price = price;
+	}
+	Phone(String name, List<String> owners) {  // for second example
+		this.name = name;
+		this.setOwners(owners);
 	}
 	
 	public String getName() {
@@ -23,6 +32,12 @@ class Phone {
 	public void setPrice(int price) {
 		this.price = price;
 	}
+	public List<String> getOwners() {
+		return owners;
+	}
+	public void setOwners(List<String> owners) {
+		this.owners = owners;
+	}
 	
 }
 
@@ -36,7 +51,34 @@ public class FlatMapTask {
 				String.format("nazvanie: %s cena bez skidki: %d", x.getName(), x.getPrice())
 //				String.format("nazvanie: %s cena so skidkoy: %s", x.getName(), (int)x.getPrice()*(0.9))
 				))
-		.forEach(s->System.out.println(s));
+		.forEach(s->System.out.println(s)); // or .forEach(System.out::println);
+		
+		
+		System.out.println();
+		System.out.println("---------Second example bellow---------");		
+		List<Phone> phones = Arrays.asList(									// создаем листы внутри листа
+				new Phone("Samsung", Arrays.asList("Buddy", "Lucy")),
+				new Phone("Samsung", Arrays.asList("Frankie", "Rosie")),
+				new Phone("Samsung", Arrays.asList("Simba", "Tilly"))
+				);
+		System.out.println("1. Without func");
+		List<String> owner1NamesList = new ArrayList<>();					
+		for (Phone ph : phones) {
+			owner1NamesList.addAll(ph.getOwners());							// в ownerNamesList добавляем всех владельцев
+		}
+		System.out.println(owner1NamesList);
+		System.out.println("2. map()+flatmap()");
+		List<String> owner2NamesList = phones.stream()
+				.map(ph -> ph.getOwners())
+				.flatMap(ow -> ow.stream())
+				.collect(Collectors.toList());
+		System.out.println(owner2NamesList);
+		owner2NamesList.forEach(System.out::print);			// Соединяет все string в одно целое
+		System.out.println();
+		System.out.println("3. only flatmap()");
+		List<String> owner3NamesList = phones.stream()
+				.flatMap(x -> x.getOwners().stream())
+				.collect(Collectors.toList());
+		System.out.println(owner3NamesList);
 	}
-	
 }
