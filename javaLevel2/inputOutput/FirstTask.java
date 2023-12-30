a79e990103d55b6489a4972255c774065308e4f4
Artemy Eranov <yuoran371@gmail.com>
2023-12-30 15:46:52 +0300

    Finish second task (Input/Output)

--- javaLevel2/inputOutput/FirstTask.java
+++ javaLevel2/inputOutput/FirstTask.java
@@ -13,6 +13,10 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/*
+ * 1. Задан файл с текстом, найти и вывести в консоль все слова,
+ * начинающиеся с гласной буквы
+ */
 public class FirstTask {
 	public static void main(String[] args) throws IOException {
 		Path path = Path.of("C:\\Users\\Oleg\\git\\AdditionalPractice\\javaLevel2\\inputOutput\\", "poem.txt");
@@ -22,12 +26,13 @@ public class FirstTask {
 		List<String> allLines = Files.readAllLines(path);
 		ArrayList<String> listOfWords = new ArrayList<>();
 		for(String line : allLines) {
-			for(String word : line.split("\\s")) {
+			for(String word : line.replaceAll("[-+.—^;:,!]+","").split("\\s")) {
 				listOfWords.add(word);
 			}
 		}
 		List<String> list = listOfWords.stream()
-				.filter(str -> str.matches("(^(?ui:[аеёиоуыэюя]).*)"))
+				.filter(str -> str.matches("(^(?ui:[аеёиоуыэюя]).*)")
+						&& str.length()>1)
 				.collect(Collectors.toList());
 		list.forEach(System.out::println);
 	}
--- javaLevel2/inputOutput/SecondTask.java
+++ javaLevel2/inputOutput/SecondTask.java
@@ -5,47 +5,50 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+/*
+ * Задан файл с текстом, найти и вывести в консоль все слова, 
+ * для которых последняя буква одного слова совпадает с первой 
+ * буквой следующего
+ */
 public class SecondTask {
 	public static void main(String[] args) throws IOException {
 		Path path = Path.of("C:\\Users\\Oleg\\git\\AdditionalPractice\\javaLevel2\\inputOutput\\", "poem.txt");
 		Stream<String> file = Files.lines(path);
-		
+
 		String str = file
 //		.map(str -> String.join("\\n", str))
 //		.map(x -> x.split("\\s"))
 //		.map(arr -> Arrays.toString(arr))
 //		.map(Object::toString)
-		.map(stri -> stri.replaceAll("[-+.—^;:,!]+",""))
+				.map(stri -> stri.replaceAll("[-+.—^;:,!]+", ""))
 //		.filter(strr -> strr.matches("(^(?ui:[аеёиоуыэюя]).*)"))
 //		.filter(str -> str.startsWith("У"))
 //		.collect(Collectors.toCollection(ArrayList::new))
 //		.collect(Collectors.toList())
-		.collect( Collectors.joining(" "))
+				.collect(Collectors.joining(" "))
 //		.forEach(System.out::println)
 		;
-		ArrayList<?> sssss = Stream.of(str)
-				.filter(strr -> strr.matches("(^(?ui:[аеёиоуыэюя]).*)"))
-				.map(Collectors::joining)
+		List<String> myList = new ArrayList<String>(Arrays.asList(str.split("\\s+")));
+//		myList.stream().forEach(System.out::println);
+		myList.stream().reduce((s1, s2) -> {
+			if(s1.charAt(s1.length()-1)==s2.charAt(0)) {
+				System.out.println(s1 + " -> " + s2);
+		}
+			return s2;
+		})
+//			.forEach(System.out::println)
+//		.collect(Collectors.toList())
 //		.collect( Collectors.joining())
-				.collect(Collectors.toCollection(ArrayList::new))
+//				.collect(Collectors.toCollection(ArrayList::new))
 //				.forEach(System.out::println)
 		;
-		System.out.println(sssss);
-//		for (Iterator iterator = list.iterator(); iterator.hasNext();) {
-//			String string = (String) iterator.next();
-//			System.out.println(string);
-//		}
-		
-		
-//		file.stream(x -> x.split("\\s")).filter(null);
-//		file.
-//		List<Object> of = List.of(file.toArray());
-//		file.filter(str -> str.);
-//		file.forEach(System.out::println);
-//		if ()
-		
+
+//		System.out.println(str);
+
 	}
 }
