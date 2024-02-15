b4173d662462b0a49f1beaa90f192143ee88d446
Artem Yuran <yuoran371@gmail.com>
2024-02-15 21:32:16 +0300

    Just starting solving "two pointers" problems

---
+++ LeetCode/twoPointers/README.md
@@ -0,0 +1,6 @@
+### LeetCode Two Pointers
+
+
+| #    | Title | Solution                         | Difficulty | Acceptance Rate |
+|------| ----- |----------------------------------|------------|-----------------|
+| 125  |[Valid Palindrome](https://leetcode.com/problems/valid-palindrome)| [Java](ValidPalindrome.java) | Easy | 46.8%           |
---
+++ LeetCode/twoPointers/ValidPalindrome.java
@@ -0,0 +1,41 @@
+package twoPointers;
+
+import java.util.ArrayList;
+
+/* 125. Valid Palindrome
+ * A phrase is a palindrome if, after converting all uppercase letters
+ * into lowercase letters and removing all non-alphanumeric characters,
+ * it reads the same forward and backward. Alphanumeric characters
+ * include letters and numbers.
+ * Given a string s, return true if it is a palindrome, or false otherwise.
+ *
+ * Example:
+ * Input: s = "A man, a plan, a canal: Panama"
+ * Output: true
+ * Explanation: "amanaplanacanalpanama" is a palindrome.
+ * */
+public class ValidPalindrome {
+    public static void main(String[] args) {
+        String s = "A man, a plan, a canal: Panama";
+        System.out.println(solution(s));
+    }
+
+    private static boolean solution(String s) {
+        StringBuilder sb = new StringBuilder();
+        ArrayList al = new ArrayList();
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char ch = s.charAt(i);
+            if (Character.isLetter(ch) && Character.isDigit(ch)) {
+                char lowerCase = Character.toLowerCase(ch);
+                sb.append(lowerCase);
+            }
+        }
+        StringBuilder reverse = new StringBuilder(sb).reverse();
+        if (sb.toString()
+                .equals(reverse.toString())) {
+            return true;
+        }
+        System.out.println(sb + " " + reverse);
+        return false;
+    }
+}
