77225a74bd9ee4d7ab714a2e9c6c48588ced9fe5
Artem Yuran <yuoran371@gmail.com>
2024-02-25 21:17:08 +0300

    Add solution O(1)

--- LeetCode/twoPointers/TrappingRainWater.java
+++ LeetCode/twoPointers/TrappingRainWater.java
@@ -43,4 +43,24 @@ public class TrappingRainWater {
         }
         return res;
     }
+
+    private static int solution2(int[] height) {
+        int l = 0;
+        int r = height.length - 1;
+        int maxL = height[l];
+        int maxR = height[r];
+        int res = 0;
+        while (l < r) {
+            if (height[l] < height[r]) {
+                l++;
+                maxL = Math.max(maxL, height[l]);
+                res += maxL - height[l];
+            } else {
+                r--;
+                maxR = Math.max(maxR, height[r]);
+                res += maxR - height[r];
+            }
+        }
+        return res;
+    }
 }
